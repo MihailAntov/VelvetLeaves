74a63c4b4ff96f5e50a77fa11ce4a3a8f2df950c
MihailAntov <114488260+MihailAntov@users.noreply.github.com>
2023-06-06 15:58:40 +0300

    Initial commit

---
+++ VelvetLeaves/VelvetLeaves.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VelvetLeaves.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
