b63a3dceb55293397b69e2dd8f4482a6bb4ea1e4
MihailAntov <114488260+MihailAntov@users.noreply.github.com>
2023-07-09 08:55:12 +0300

    AddedInitialMigration

---
+++ VelvetLeaves/VelvetLeaves.Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VelvetLeaves.Data
+{
+    public class VelvetLeavesDbContext : IdentityDbContext
+    {
+        public VelvetLeavesDbContext(DbContextOptions<VelvetLeavesDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
