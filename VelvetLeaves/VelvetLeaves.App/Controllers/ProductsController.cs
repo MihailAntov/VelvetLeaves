9f47f78b31d5675c16e69cde85f85cadf9044440
MihailAntov <114488260+MihailAntov@users.noreply.github.com>
2023-07-17 12:53:13 +0300

    Added admin controllers

---
+++ VelvetLeaves/VelvetLeaves.App/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VelvetLeaves.App.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
