de9c812c708dbe46599c28256a86958a59ba307e
MihailAntov <114488260+MihailAntov@users.noreply.github.com>
2023-07-28 22:38:42 +0300

    Added image previews to edit pages

---
+++ VelvetLeaves/VelvetLeaves.App/wwwroot/js/uploadImage.js
@@ -0,0 +1,25 @@
+﻿const batchUpload = document.querySelector('#formFile');
+batchUpload.addEventListener('change', visualizeFile);
+
+
+function visualizeFile(e) {
+    const input = e.currentTarget;
+    const gallery = document.querySelector('.image-preview-gallery');
+
+
+        let newImage = document.createElement('img');
+        newImage.classList.add('img-fluid');
+        newImage.classList.add('img-preview');
+        newImage.classList.add('col-2');
+
+
+    newImage.src = URL.createObjectURL(input.files[0]);
+    gallery.replaceChildren();
+    gallery.appendChild(newImage);
+ }
+
+   
+
+
+
+
